ff6f4a9eda84b0ac47e15d2a0ae9c11f8b8ce4bf
blahblahblah1920 <pranavabishaimoses@gmail.com>
2024-01-23 17:50:08 -0600

    Updated logginf

--- src/exception.py
+++ src/exception.py
@@ -18,4 +18,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-logger.logging.warning("Is it working????")
+# logger.logging.warning("Is it working????")
